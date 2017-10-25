8f09ec894dc246af3b900851475ad5e29b12eecb
Rob <ravenstudiosrob@yahoo.com>
2017-10-25 11:14:36 -0500

    timer now goes boh ways

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ console.log("pomodoro");
 let workTimer = 1;
 let breakTimer = 1;
 let clock = 0;
-
+let timerIsRunning = false;
 $(()=>{
   console.log("jquery");
 
@@ -35,39 +35,72 @@ $(()=>{
 
   $("#clock").click(()=>{
     breakTimer++;
-    timer(workTimer);
+    timer();
 
   });
 });
 
-function timer(timeIn){
+function timer(){
 
-
+  //if timer is running than pause and let user change timer settings
+  //if settings have changed then restart timer with those setings,
+  //else resume timer
   let minutes;
   let seconds = 10;
   let displayString;
+  let timeToWork = true;
 
-
-    minutes = timeIn;
+    minutes = 0;
 
 
   let clock = setInterval(()=>{
-    if(seconds <= 1 && minutes <= 0){
-      clearInterval(clock);
-      $("#clock").html("0:00");
-    }
-    seconds --;
+    // if(seconds <= 2 && minutes <= 0){
+    //   $("#clock").html("0:00");
+    //   clearInterval(clock);
+    //
+    // }
 
-    if(seconds <= 0){
-      minutes --;
-      seconds = 59;
-    }
-    if(seconds < 10){
-      displayString = minutes + ":0" + seconds;
+    if(timeToWork){
+
+      if(seconds <= 2 && minutes <= 0){
+        timeToWork = false;
+        minutes = 0;
+        seonds = 0;
+      }
+
+      seconds --;
+      if(seconds <= 0){
+        minutes --;
+        seconds = 59;
+      }
+      if(seconds < 10){
+        displayString = minutes + ":0" + seconds;
+      }
+      else{
+        displayString = minutes + ":" + seconds;
+      }
     }
     else{
-      displayString = minutes + ":" + seconds;
+      if(minutes === breakTimer){
+        timeToWork = true;
+        minutes = workTimer;
+        seconds = 0;
+      }
+
+      seconds++;
+      if(seconds > 59){
+        minutes ++;
+        seonds = 0;
+      }
+      if(seconds < 10){
+        displayString = minutes + ":0" + seconds;
+      }
+      else{
+        displayString = minutes + ":" + seconds;
     }
+  }
+
+
 
     $("#clock").html(displayString);
   }, 1000);
